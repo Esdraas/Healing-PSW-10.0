19de655ae2d750ea8708ebf41868d891f6ee6060
Esdraas <esdrasrw@gmail.com>
2024-04-21 16:33:20 -0300

    Finalizar Minhas consultas (Médico)

--- medico/views.py
+++ medico/views.py
@@ -3,7 +3,8 @@ from .models import Especialidades, DadosMedico, is_medico, DatasAbertas
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.messages import constants
-from datetime import datetime
+from datetime import datetime, timedelta
+from paciente.models import Consulta
 
 def cadastro_medico(request):
 
@@ -60,7 +61,7 @@ def abrir_horario(request):
         dados_medicos = DadosMedico.objects.get(user=request.user)
         datas_abertas = DatasAbertas.objects.filter(user=request.user)
         return render(request, 'abrir_horario.html', {'dados_medicos': dados_medicos, 'datas_abertas': datas_abertas})
-        
+
     elif request.method == "POST":
         data = request.POST.get('data')
 
@@ -80,3 +81,17 @@ def abrir_horario(request):
 
         messages.add_message(request, constants.SUCCESS, 'Horário cadastrado com sucesso.')
         return redirect('/medicos/abrir_horario')
+
+def consultas_medico(request):
+
+    if not is_medico(request.user):
+        messages.add_message(request, constants.WARNING, 'Somente médicos podem acessar essa página.')
+        return redirect('/usuarios/sair')
+    
+    hoje = datetime.now().date()
+
+    consultas_hoje = Consulta.objects.filter(data_aberta__user=request.user).filter(data_aberta__data__gte=hoje).filter(data_aberta__data__lt=hoje + timedelta(days=1))
+    consultas_restantes = Consulta.objects.exclude(id__in=consultas_hoje.values('id'))
+
+    return render(request, 'consultas_medico.html', {'consultas_hoje': consultas_hoje, 'consultas_restantes': consultas_restantes, 'is_medico': is_medico(request.user)})
+
